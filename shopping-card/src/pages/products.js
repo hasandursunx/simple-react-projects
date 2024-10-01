aa4e1434923a5f80613d05a1128013d086c93f15
Hasan <hasandursunx@gmail.com>
2024-10-01 23:20:52 +0300

    redux system added

---
+++ shopping-card/src/pages/products.js
@@ -0,0 +1,73 @@
+import imageProduct1 from '../assets/images/1.png'
+import imageProduct2 from '../assets/images/2.png'
+import imageProduct3 from '../assets/images/3.png'
+import imageProduct4 from '../assets/images/4.png'
+import imageProduct5 from '../assets/images/5.png'
+import imageProduct6 from '../assets/images/6.png'
+import imageProduct7 from '../assets/images/7.png'
+import imageProduct8 from '../assets/images/8.png'
+export const products = [
+    {
+        id: 1,
+        name: "Product A",
+        price: 101,
+        image: imageProduct1,
+        description: 'Laboris nisi aliqua non duis nulla incididunt deserunt non labore id sint.',
+        slug: "product-a"
+    },
+    {
+        id: 2,
+        name: "Product B",
+        price: 201,
+        image: imageProduct2,
+        description: 'Laboris nisi aliqua non duis nulla incididunt deserunt non labore id sint.',
+        slug: "product-b"
+    },
+    {
+        id: 3,
+        name: "Product C",
+        price: 301,
+        image: imageProduct3,
+        description: 'Laboris nisi aliqua non duis nulla incididunt deserunt non labore id sint.',
+        slug: "product-c"
+    },
+    {
+        id: 4,
+        name: "Product D",
+        price: 401,
+        image: imageProduct4,
+        description: 'Laboris nisi aliqua non duis nulla incididunt deserunt non labore id sint.',
+        slug: "product-d"
+    },
+    {
+        id: 5,
+        name: "Product E",
+        price: 501,
+        image: imageProduct5,
+        description: 'Laboris nisi aliqua non duis nulla incididunt deserunt non labore id sint.',
+        slug: "product-e"
+    },
+    {
+        id: 6,
+        name: "Product F",
+        price: 601,
+        image: imageProduct6,
+        description: 'Laboris nisi aliqua non duis nulla incididunt deserunt non labore id sint.',
+        slug: "product-f"
+    },
+    {
+        id: 7,
+        name: "Product G",
+        price: 701,
+        image: imageProduct7,
+        description: 'Laboris nisi aliqua non duis nulla incididunt deserunt non labore id sint.',
+        slug: "product-g"
+    }, {
+        id: 8,
+        name: "Product H",
+        price: 801,
+        image: imageProduct8,
+        description: 'Laboris nisi aliqua non duis nulla incididunt deserunt non labore id sint.',
+        slug: "product-h"
+    },
+]
