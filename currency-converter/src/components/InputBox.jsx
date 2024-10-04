d5d254b604baf795108fe1822d5f6d3f627aa617
Hasan <hasandursunx@gmail.com>
2024-10-04 22:15:15 +0300

    ınputbox component created

---
+++ currency-converter/src/components/InputBox.jsx
@@ -0,0 +1,51 @@
+import React from 'react'
+
+const InputBox = ({
+    label,
+    amount,
+    onAmountChange,
+    onCurrencyChange,
+    currencyOption = [],
+    selectedCurrency = "usd",
+    amountDisabled = false,
+    currencyDisabled = false,
+    className = ""
+
+}) => {
+    return (
+        <div className={`${className} bg-white p-3 rounded-lg text-sm flex`}>
+            <div className='w-1-2'>
+                <label className='text-black/40 mb-2 inline-block' htmlFor="">{label}</label>
+                <input
+                    type="text"
+                    className='outline-none w-full bg-transparent py-1.5'
+                    placeholder='Amount'
+                    disabled={amountDisabled}
+                    value={amount}
+                    onChange={(e) => (Number(onAmountChange && (e.target.value)))}
+                />
+            </div>
+            <div className='w-1/2 flex flex-wrap justify-end text-right'>
+                <p className='text-black/40 mb-2 w-full'>Currency Type</p>
+                <select
+                    name="" i
+                    id=""
+                    className='rounded-lg px-1 py-1 bg-gray-100 cursor-pointer outline-none'
+                    value={selectedCurrency}
+                    onChange={(e) => { onCurrencyChange && onCurrencyChange(e.target.value) }}
+                    disabled={currencyDisabled}
+
+                >
+                    {
+                        currencyOptions.map((currency) => (
+                            <option key={currency} value={currency} > {currency} </option>
+                        ))
+                    }
+
+                </select>
+            </div>
+        </div>
+    )
+}
+
+export default InputBox
