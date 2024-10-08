5251a4d1858cf63ae1df61d46ad86dce3fa20d0a
Hasan <hasandursunx@gmail.com>
2024-10-08 08:48:54 +0300

    project finished

--- weather-app/src/App.jsx
+++ weather-app/src/App.jsx
@@ -1,15 +1,43 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
+import axios from 'axios'
+import Weather from './components/Weather'
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  const [data, setData] = useState({})
+  const [location, setLocation] = useState("")
+
+  const API_KEY = "89f296a01b80badb8fedf52b3110b5d1"
+  const url = `https://api.openweathermap.org/data/2.5/weather?q=${location}&units=metric&appid=${API_KEY} `
+
+  const searchLocation = (e) => {
+    if (e.key == "Enter") {
+      axios.get(url)
+        .then(response => {
+          setData(response.data)
+          console.log(response.data)
+        })
+      setLocation("")
+    }
+  }
   return (
-    <>
+    <div className='w-full h-full relative'>
+      <div className='text-center p-4'>
+        <input
+          type="text"
+          className='py-3 px-6 w-[700px] text-lg rounded-3xl border border-gray-200 text-gray-600 
+                   placeholder:text-gray-400 focus: outline-none bg-white-600/100 shadow-md'
+          placeholder='Enter Location'
+          value={location}
+          onChange={(e) => setLocation(e.target.value)}
+          onKeyDownCapture={searchLocation}
+        />
 
-    </>
+      </div>
+
+      <Weather weatherData={data} />
+    </div>
   )
 }
 
