327792393d7857d7c3621ab7d91d142c3bd9c06a
Hasan <hasandursunx@gmail.com>
2024-10-13 22:25:34 +0300

    project finished

--- multi-language-i18next/src/App.jsx
+++ multi-language-i18next/src/App.jsx
@@ -1,11 +1,29 @@
+import { Trans, useTranslation } from 'react-i18next'
 import './App.css'
+import LanguageSelector from './components/LanguageSelector'
 
 function App() {
+  const { t } = useTranslation()
+
+  const { line1, line2 } = t("description", { lang: "React" })
 
   return (
     <div className='container'>
-      <h1>Hello Welcome</h1>
-    </div>
+      <LanguageSelector />
+      <h1>{t("greeting")}</h1>
+      <span>
+        <Trans
+          i18nKey={line1}
+          values={{
+            Lang: "React"
+          }}
+          components={{ 1: <mark><b /></mark> }}
+        >
+
+        </Trans>
+      </span >
+      <span>{line2}</span>
+    </div >
   )
 }
 
--- multi-language-i18next/src/main.jsx
+++ multi-language-i18next/src/main.jsx
@@ -1,10 +1,13 @@
-import { StrictMode } from 'react'
+import React, { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
+import './i18n.js'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
-    <App />
+    <React.Suspense fallback='loading...'>
+      <App />
+    </React.Suspense>
   </StrictMode>,
 )
