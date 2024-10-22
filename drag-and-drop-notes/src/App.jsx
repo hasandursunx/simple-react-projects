95b08b1afd76cbcb01613de909d076112d236304
Hasan <hasandursunx@gmail.com>
2024-10-22 18:41:24 +0300

    project finished

--- drag-and-drop-notes/src/App.jsx
+++ drag-and-drop-notes/src/App.jsx
@@ -1,9 +1,24 @@
+import { useState } from 'react'
 import './App.css'
+import Notes from './components/Notes'
 
 function App() {
+  const [notes, setNotes] = useState([
+    {
+      id: 1,
+      text: " Bu gün 1 tane react projesi tamamlanacak ve github'a eklenecek."
+    },
+    {
+      id: 2,
+      text: "20 Tane ingilizce kelime araştırılacak ve örnek cümleler kurulacak"
+    },
 
+
+  ])
   return (
-    <div>test</div>
+    <div>
+      <Notes notes={notes} setNotes={setNotes} />
+    </div >
   )
 }
 
---
+++ drag-and-drop-notes/src/components/Notes.jsx
@@ -0,0 +1,120 @@
+import React, { useEffect, useRef } from 'react'
+import Note from './Note'
+import { createRef } from 'react';
+
+const Notes = ({ notes = [], setNotes = () => { } }) => {
+    useEffect(() => {
+        const savedNotes = JSON.parse(localStorage.getItem("notes")) || [];
+
+        const updateNotes = notes.map((note) => {
+            const savedNote = savedNotes.find((n) => n.id === note.id);
+            if (savedNote) {
+                return { ...note, position: savedNote.position }
+            } else {
+                const position = determineNewPosition();
+                return { ...note, position }
+            }
+
+        })
+        setNotes(updateNotes)
+        localStorage.setItem("notes", JSON.stringify(updateNotes))
+    }, [notes.length])
+
+    const noteRefs = useRef([]);
+
+    const determineNewPosition = () => {
+        const maxX = window.innerWidth - 250;
+        const maxY = window.innerHeight - 250;
+
+        return {
+            x: Math.floor(Math.random() * maxX),
+            y: Math.floor(Math.random() * maxY)
+        }
+    };
+
+    const handleDragStart = (note, e) => {
+        const { id } = note;
+        const noteRef = noteRefs.current[id].current;
+        const rect = noteRef.getBoundingClientRect();
+        const offsetX = e.clientX - rect.left;
+        const offsetY = e.clientY - rect.top;
+
+        const startPos = note.position;
+
+        const handleMouseMove = (e) => {
+            const newX = e.clientX - offsetX;
+            const newY = e.clientY - offsetY;
+
+            noteRef.style.left = `${newX}px`
+            noteRef.style.top = `${newY}px`
+        }
+        const handleMouseUp = () => {
+            document.removeEventListener("mousemove", handleMouseMove)
+            document.removeEventListener("mouseup", handleMouseUp)
+
+            const finalRect = noteRef.getBoundingClientRect()
+            const newPosition = { x: finalRect.left, y: finalRect.top }
+
+            if (checkForOverLap(id)) {
+                noteRef.style.left = `${startPos.x}px`
+                noteRef.style.top = `${startPos.y}px`
+            } else {
+                updateNotePosition(id, newPosition);
+            };
+
+        }
+        document.addEventListener("mousemove", handleMouseMove)
+        document.addEventListener("mouseup", handleMouseUp)
+
+        console.log(offsetX)
+        console.log(rect)
+    };
+
+    const checkForOverLap = (id) => {
+        const currentNoteRef = noteRefs.current[id].current;
+        const currentRect = currentNoteRef.getBoundingClientRect();
+
+        return notes.some((note) => {
+            if (note.id === id) return false
+
+            const otherNoteRef = noteRefs.current[note.id].current
+            const otherRect = otherNoteRef.getBoundingClientRect();
+
+            const overlap = !(
+                currentRect.right < otherRect.left ||
+                currentRect.left > otherRect.right ||
+                currentRect.bottom < otherRect.top ||
+                currentRect.top > otherRect.bottom
+            )
+
+            return overlap;
+        })
+    }
+
+    const updateNotePosition = (id, newPosition) => {
+        const updateNotes = notes.map(note => note.id === id ? { ...note, position: newPosition } : note)
+        setNotes(updateNotes)
+        localStorage.setItem("notes", JSON.stringify(updateNotes));
+    }
+    return (
+        <div>
+            {notes.map((note) => {
+                return <Note
+                    key={note.id}
+                    ref={
+                        noteRefs.current[note.id]
+                            ? noteRefs.current[note.id]
+                            : (noteRefs.current[note.id] = createRef())
+                    }
+                    content={note.text}
+                    initialPosition={note.position}
+                    onMouseDown={(e) => handleDragStart(note, e)}
+                />
+            })
+
+            }
+        </div>
+    )
+}
+
+export default Notes
